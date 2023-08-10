030547c60f6befd680edf2248262d1f4e3c94c56
kaimo <kaimo313@foxmail.com>
2023-08-11 02:01:34 +0800

    update：67 # 对象的处理

--- doc/67 # 对象的处理.js
+++ doc/67 # 对象的处理.js
@@ -1,10 +1,3 @@
-// 实现一个 http 服务器 客户端会发送请求 GET POST
-
-// 要处理不同的请求体的类型
-// 1. 表单格式（formData `a=1&b=2`），可以直接通信不会出现跨域问题
-// 2. JSON （`"{"kaimo":"313"}"`）
-// 3. 文件格式 （二进制）
-
 const http = require("http");
 const url = require("url");
 const querystring = require("querystring");
@@ -14,35 +7,42 @@ let server = http.createServer();
 server.on("request", (req, res) => {
     let { pathname } = url.parse(req.url);
 
+    // 1）配置跨域
+    // 配置跨域头允许任何网站访问
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    // 允许携带header
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
+    // 默认支持 get 和 post
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST");
+
+    // 遇到 OPTIONS 预检请求，直接成功即可
     if (req.method === "OPTIONS") {
         res.statusCode = "200";
         res.end();
     }
-
+    // 2）解析请求体
     const arr = [];
     req.on("data", (chunk) => {
         arr.push(chunk);
     });
     req.on("end", () => {
-        if (pathname === "/login" && req.method == "POST") {
-            let result = Buffer.concat(arr).toString();
-            console.log(result);
-            if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
-                let obj = querystring.parse(result, "&", "=");
-                console.log(obj);
-                res.setHeader("Content-Type", "application/json");
-                res.end(JSON.stringify(obj));
-            }
+        let result = Buffer.concat(arr).toString();
+        console.log("result---->", result);
+        let obj;
+        if (req.headers["content-type"] === "application/x-www-form-urlencoded") {
+            obj = querystring.parse(result, "&", "=");
+        } else if (req.headers["content-type"] === "application/json") {
+            obj = JSON.parse(result);
         }
-        console.log(req.method, pathname);
+        console.log("obj---->", obj);
+        // 3）根据不同路径返回对应内容
+        if (pathname === "/login" && req.method == "POST") {
+            res.setHeader("Content-Type", "application/json");
+            res.end("登录成功");
+        }
         if (pathname === "/regist" && req.method == "POST") {
-            let result = Buffer.concat(arr).toString();
-            console.log(result);
-            if (req.headers["content-type"] === "application/json") {
-                let obj = JSON.parse(result);
-                res.setHeader("Content-Type", "application/json");
-                res.end(JSON.stringify(obj));
-            }
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(obj));
         }
     });
 });
